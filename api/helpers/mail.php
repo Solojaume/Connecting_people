59ba89faacdbd75eaff0d434e9a6df493772533d
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-04-13 15:28:21 +0200

    mail

---
+++ api/helpers/mail.php
@@ -0,0 +1,8 @@
+<?php
+use Symfony\Component\Mailer\Mailer;
+    $p="";
+    class MailHelper {
+        public static function sendMail($email="jaumeponsorti@gmail.com",$enlace="",$contraseña=""){
+
+        }
+    }
