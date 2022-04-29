1ecedaf2b9fc9c0b3c936f3d35d361b2d5b68b72
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-04-29 17:56:47 +0200

    gvf

--- frontend/src/app/public/login/login.component.ts
+++ frontend/src/app/public/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, } from '@angular/forms';
+import { UsuarioAPP } from 'src/app/core/models/usuario/usuario-app.model';
+import { ApiService } from 'src/app/core/shared/services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +9,33 @@ import {FormControl, FormGroup, } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  datos!:String;
-  constructor() { 
+  datos!:any;
+  usuario:UsuarioAPP= <UsuarioAPP>{id:-1,nombre:"", token:"",rol:0};
+  us!:UsuarioAPP;
+  error:{error:string}={error:""};
+  constructor(private apiService:ApiService) { 
   }
   formularioLogin = new FormGroup({
     mail: new FormControl(''),
     password: new FormControl('')
   });
 
+  usuarioApp(usuario:UsuarioAPP){
+    this.usuario=usuario;
+
+  }
+
   submit() {
-    this.datos=`
-    Mail=${this.formularioLogin.value.mail}
-    Password=${this.formularioLogin.value.password}
-    `;
-    console.log(this.formularioLogin.value.nombre);
+    let mail = this.formularioLogin.value.mail;
+    let password = this.formularioLogin.value.password;
+
+    this.apiService.usuarioLogin(password,mail).subscribe(usuario=> { 
+      //console.log(usuario);
+      this.usuario=usuario;
+      });
+    //this.datos=JSON.stringify(this.us);
+    //console.log(this.us); 
+    console.log(this.usuario);
   }
   buttonLogueame={
     nombre: "Logueame",
