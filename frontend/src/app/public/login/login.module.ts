7cbfe38a8b584530705e4b93635019068f7a66d4
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-05-04 16:23:28 +0200

    Guardado de token añadido falta probar

--- frontend/src/app/public/login/login.module.ts
+++ frontend/src/app/public/login/login.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from 'src/app/core/shared/shared.module';
-import { ApiService } from 'src/app/core/shared/services/api.service';
 
 
 
