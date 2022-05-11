4c5926d9cdfa3608e5cef4ca161122c9a15d300f
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-05-11 16:48:35 +0200

    Añadida pagina politicas

---
+++ frontend/src/app/public/politicas/terminos-condiciones/terminos-condiciones-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TerminosCondicionesComponent } from './terminos-condiciones.component';
+
+const routes: Routes = [
+  {path:'',component:TerminosCondicionesComponent},
+  {path:'terminos-condiciones',component:TerminosCondicionesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TerminosCondicionesRoutingModule { }
