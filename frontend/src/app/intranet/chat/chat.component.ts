c5b08097c09cf4241c3a9884f75909060e8cf09a
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-05-27 13:57:07 +0200

    chat component

--- frontend/src/app/intranet/chat/chat.component.ts
+++ frontend/src/app/intranet/chat/chat.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ChatMessageDto } from 'src/app/core/models/chatMessageDto';
+import { TokenStorageService } from 'src/app/core/shared/services/token-storage.service';
+import { WebSocketService } from 'src/app/core/shared/services/web-socket.service';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+
+  constructor(public webSocketService: WebSocketService,private token:TokenStorageService) { }
 
   ngOnInit(): void {
+    this.webSocketService.openWebSocket();
   }
 
+  ngOnDestroy(): void {
+    this.webSocketService.closeWebSocket();
+  }
+
+  sendMsg(sendForm: NgForm) {
+    const chatMessageDto = new ChatMessageDto(this.token.getUser().nombre, sendForm.value.message);
+    this.webSocketService.sendMessage(chatMessageDto);
+    console.log(sendForm);
+    //sendForm.controls.message.reset();
+  }
 }
+
