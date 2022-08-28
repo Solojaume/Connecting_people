071102fd1712f6b19c69193fa63b41e5fdaab5cb
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-08-28 12:27:54 +0200

    Subir imagenes arreglado 100 por 100

--- frontend/src/app/core/shared/components/file-upload/file-upload.module.ts
+++ frontend/src/app/core/shared/components/file-upload/file-upload.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFileUploaderModule } from 'angular-file-uploader';
-import { ImageUploaderComponent } from './image-uploader/image-uploader.component';
 import { ServicesModule } from '../../services/services.module';
 import { ImagesComponent } from './images/images.component';
 import { NewImageUploaderComponent } from './new-image-uploader/new-image-uploader.component';
@@ -10,7 +9,6 @@ import { NewImageUploaderComponent } from './new-image-uploader/new-image-upload
 
 @NgModule({
   declarations: [
-    ImageUploaderComponent,
     ImagesComponent,
     NewImageUploaderComponent,
   ],
@@ -21,7 +19,7 @@ import { NewImageUploaderComponent } from './new-image-uploader/new-image-upload
   ],
   exports:[
     AngularFileUploaderModule,
-    ImageUploaderComponent,
+ 
     ImagesComponent
   ],
   providers:[]
