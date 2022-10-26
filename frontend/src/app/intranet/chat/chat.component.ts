472837e8a58a8533a6d72a78e5a5344adbfa00bd
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-10-26 11:53:49 +0200

    Añadido que cuando hay mensaje vacio alla error

--- frontend/src/app/intranet/chat/chat.component.ts
+++ frontend/src/app/intranet/chat/chat.component.ts
@@ -41,21 +41,22 @@ export class ChatComponent implements OnInit {
     @Inject(LOCALE_ID) public locale: string
   ) {}
   public typeofUsuario2!: boolean;
- public claseQueUsaImput: string = "form-control";
+  public claseQueUsaImput: string = 'form-control';
+  public mensajeVacio: boolean = false;
   ngOnInit(): void {
     this.socketService.setPage('chat');
     this.typeofUsuario2 =
       typeof this.socketService.chatUsar.match_id_usu2 !== 'undefined';
     this.formularioEnvio.valueChanges.subscribe((x) => {
       // console.log('x:', x.message);
-      let y=""+x.message;
-      
-      if (y.length <= 1000) {
-        this.claseQueUsaImput = "form-control";
+      let y = '' + x.message;
+
+      if (y.length <= 1000 && y.length > 0) {
+        this.claseQueUsaImput = 'form-control';
         this.setTyping('' + x.message);
-      }else{
+      } else {
         this.claseQueUsaImput = 'form-control form-control-error';
-      } 
+      }
     });
     this.cdkScrollable.scrollTo({ bottom: 100 });
   }
@@ -67,10 +68,6 @@ export class ChatComponent implements OnInit {
   }
 
   sendMessage() {
-    //console.log("Chat Usar:",this.chatUsar)
-    //const date = new Date();
-    //Sirve para obtener la fecha  en un formato concreto
-    //alert(formatDate(date,"hh:mm",this.locale));
     let chatMessageDto = new MensajeModel(
       this.token.getUser().id,
       this.formularioEnvio.value.message,
@@ -81,19 +78,27 @@ export class ChatComponent implements OnInit {
       formatDate(new Date(), 'yyyy-mm-dd HH:mm:ss', this.locale)
     );
     let chatUsar = this.socketService.chatUsar;
-    this.socketService.mensajes[chatUsar.match_position].push(chatMessageDto);
     let match_id_usu2 = chatUsar.match_id_usu2;
+    if (
+      this.formularioEnvio.value.message.length === 0 ||
+      this.formularioEnvio.value.message == null
+    ) {
+      this.mensajeVacio = true;
+    } else {
+      this.mensajeVacio = false;
+      this.socketService.mensajes[chatUsar.match_position].push(chatMessageDto);
 
-    this.socketService.emit('send private message', {
-      token: this.token.getUser().token,
-      mensage: chatMessageDto,
-      usu_2: match_id_usu2.id,
-    });
+      this.socketService.emit('send private message', {
+        token: this.token.getUser().token,
+        mensage: chatMessageDto,
+        usu_2: match_id_usu2.id,
+      });
+    }
+
     //Esto sirve para que si un usuario envia un mensage a un macth le aparezca como chat
     this.socketService.modify_conection_status();
-
-    this.formularioEnvio.value.message = '';
     this.formularioEnvio.reset();
+    this.formularioEnvio.value.message = '';
     this.setTyping('');
     this.cdkScrollable.scrollTo({
       bottom: 0,
