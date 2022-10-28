117cec28844ab43fee21d53cc5dfbc98a5d550c5
Solojaume <43333015+Solojaume@users.noreply.github.com>
2022-10-28 13:54:26 +0200

    Despliegue parte 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  serverSocket: 'ws://127.0.0.1:3000',
+  apiBase:"http://localhost/connectingpeople/api/web/",
+  imagenesBase:"http://localhost/connectingpeople/api/imagenes/",
+  intervalo_tiempo_reescaneo_chat_match:10000
 };
