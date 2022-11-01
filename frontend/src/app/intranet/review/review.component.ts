365c54a6ed9be9517c7b57159816d9d36c248f92
jaume <jaumeponsorti@gmail.com>
2022-11-01 12:41:18 +0100

    Se puede cambiar  la review

--- frontend/src/app/intranet/review/review.component.ts
+++ frontend/src/app/intranet/review/review.component.ts
@@ -9,44 +9,54 @@ import { AspectoService } from 'src/app/core/shared/services/hacer_review/aspect
 export class ReviewComponent implements OnInit {
 
   constructor(public aspecto: AspectoService) { }
-  tipo_review ='simple';
-  currentRate:Array<atributo>=[{puntuacion:1,max:5},{puntuacion:1,max:1}];
-  readonly:boolean=false;
+  tipo_review = 'simple';
+  currentRate: Array<atributo> = [{ puntuacion: 1, max: 5 }, { puntuacion: 1, max: 1 }];
+  readonly: boolean = false;
   ngOnInit(): void {
-    
+
   }
-  cambiar(tipo:string){
+  cambiar(tipo: string) {
     this.tipo_review = tipo;
     switch (tipo) {
       case 'avanzado':
-        this.readonly=true;
+        this.readonly = true;
         break;
-    
+
       default:
-        this.readonly=false;
+        this.readonly = false;
         break;
     }
   }
-  
-  calcularMedia(){
-    let suma_puntuaciones=0;
-    let contador=this.aspecto.puntuaciones_review.length-1;
+
+  calcularMedia() {
+    let suma_puntuaciones = 0;
+    let contador = this.aspecto.puntuaciones_review.length - 1;
     let aspectoGeneral = this.aspecto.aspectos[0];
     for (let index = 1; index < this.aspecto.puntuaciones_review.length; index++) {
       const puntuacion_r = this.aspecto.puntuaciones_review[index];
       const aspecto = this.aspecto.aspectos[index];
       let puntuacion = puntuacion_r.puntuaciones_review_puntuacion;
-      puntuacion = (puntuacion*aspectoGeneral.puntuacion_maxima)/aspecto.puntuacion_maxima;
-      suma_puntuaciones= suma_puntuaciones+puntuacion;
+      puntuacion = (puntuacion * aspectoGeneral.puntuacion_maxima) / aspecto.puntuacion_maxima;
+      suma_puntuaciones = suma_puntuaciones + puntuacion;
     }
-    this.aspecto.puntuaciones_review[0].puntuaciones_review_puntuacion = suma_puntuaciones/contador;
-    
+    this.aspecto.puntuaciones_review[0].puntuaciones_review_puntuacion = suma_puntuaciones / contador;
   }
 
+  modificarPuntuacionesAvanzadas() {
+    let suma_puntuaciones = 0;
+    let puntuacion_gen = this.aspecto.puntuaciones_review[0].puntuaciones_review_puntuacion;
+    let aspectoGeneral = this.aspecto.aspectos[0];
+    for (let index = 1; index < this.aspecto.puntuaciones_review.length; index++) {
+      const aspecto = this.aspecto.aspectos[index];
+      let puntuacion = (puntuacion_gen * aspecto.puntuacion_maxima) / aspectoGeneral.puntuacion_maxima;
+      this.aspecto.puntuaciones_review[index].puntuaciones_review_puntuacion = puntuacion;
+    }
+
+  }
 }
 
-interface atributo{
-  puntuacion:number,
-  max:number,
-  min?:number
+interface atributo {
+  puntuacion: number,
+  max: number,
+  min?: number
 }
